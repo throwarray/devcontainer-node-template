5e6e9721d6bd62433feab7504423187982bd907c
User Name <user@example.com>
2026-02-11 09:26:41 +0000

    FIXES .gitconfig

--- .github/repo.sh
+++ .github/repo.sh
@@ -3,7 +3,7 @@ git config user.name "${GIT_NAME:-User Name}"
 git config user.email "${GIT_EMAIL:-user@example.com}"
 
 git init
-git remote set-url "${GIT_REMOTE:-https://github.com/throwarray/devcontainer-node-template.git}" 2>/dev/null
+git remote set-url origin "${GIT_REMOTE:-https://github.com/throwarray/devcontainer-node-template.git}" 2>/dev/null
 
 echo "Git setup complete."
 echo "Using remote: $(git remote get-url origin)"
